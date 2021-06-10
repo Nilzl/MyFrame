a66689f9b7e00b4ef95a24288b60fae1843e4b00
lzl1735 <173592263@qq.com>
2021-06-10 18:31:36 +0800

    命令模式实现

--- Assets/Scripts/Command.cs
+++ Assets/Scripts/Command.cs
@@ -10,7 +10,10 @@ class Command
 {
     public static List<Command> commands = new List<Command>(); //命令列表
 
-    public Command curCommand;
+    public static Command curCommand = null;//当前命令
+
+    //记一下撤销到开始时候的标记
+    private bool isBegin = false;
 
     public Command()
     {
@@ -19,31 +22,60 @@ class Command
 
     //命令执行
     public virtual void excute() {
-        curCommand = this;
-        commands.Add(curCommand);
-        // 创建新命令时清楚当前命令后的缓存命令
-        for (int i = 0; i < commands.Count; i++)
+        bool isExist = false;
+
+        //先判断当前执行的命令是否在命令列表内
+        foreach(Command c in commands)
         {
-            if (commands[i] == curCommand)
+            if (c == this)
             {
-                for (int j = i + 1; j < commands.Count; j++)
+                isExist = true;
+                curCommand = this;
+            }    
+        }
+
+        if (!isExist)
+        {
+            // 创建新命令时先清除当前命令后的缓存命令
+            for (int i = 0; i < commands.Count; i++)
+            {
+                if (commands[i] == curCommand)
                 {
-                    commands.RemoveAt(j);
+                    
+                    //撤销到开始位置时直接清空命令列表
+                    if (curCommand.isBegin)
+                    {
+                        commands.Clear();
+                    }
+                    int len = commands.Count - 1 - i;
+                    while (len>0)
+                    {
+                        commands.RemoveAt(i + 1);
+                        len--;
+                    }
+                    break;
                 }
             }
+            curCommand = this;
+            commands.Add(curCommand);      
         }
+        
     } 
 
     //命令撤销 ctr Z
     public virtual void undo() {
+
         for(int i = 0; i < commands.Count; i++)
         {
             if(commands[i] == curCommand)
             {
                 if (i > 0)
                     curCommand = commands[i - 1];
-                else
-                    return;
+                else if(i == 0) //撤销到开始位置时，curCommand仍然指向第一个元素，标记一下
+                {
+                    curCommand.isBegin = true;
+                }
+                return;
             }
         }
     }
@@ -54,29 +86,38 @@ class Command
         for (int i = 0; i < commands.Count; i++)
         {
             if (commands[i] == curCommand)
-            { 
-                if (i< commands.Count - 1)
+            {
+                //撤销到开始位置，重做直接执行当前元素的excute方法，下一次再往后推
+                if (curCommand.isBegin)
+                {
+                    curCommand.excute();
+                    curCommand.isBegin = false;
+                }
+                else if (i < commands.Count - 1)
+                {
                     curCommand = commands[i + 1];
-                else
-                    return;
+                    curCommand.excute();
+                }
+                return;
             }
         }
-        curCommand.excute();
     } 
 }
 
+//增加游戏对象命令
 class AddCommand : Command
 {
+    private GameObject prefObj;
     private GameObject curObj;
 
     public AddCommand(GameObject obj):base() {
-        curObj = obj;
+        prefObj = obj;
     }
 
     public override void excute()
     {
         base.excute();
-        GameObject.Instantiate(curObj);
+        curObj = GameObject.Instantiate(prefObj);
     }
 
     public override void undo()
@@ -91,6 +132,7 @@ class AddCommand : Command
     }
 }
 
+//移动命令
 class MoveCommand : Command
 {
     public MoveCommand(MoveCommand unit, int x, int y) : base()
--- Assets/Scripts/testCommand.cs
+++ Assets/Scripts/testCommand.cs
@@ -1,26 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class testCommand : MonoBehaviour
 {
     public GameObject prefObj;
 
-    private Command command;
-
     private void Start()
     {
         prefObj = Resources.Load<GameObject>("SpriteTest");
+        GameObject.Find("add").GetComponent<Button>().onClick.AddListener(AddButtonEvent);
+        GameObject.Find("undo").GetComponent<Button>().onClick.AddListener(undoButtonEvent);
+        GameObject.Find("redo").GetComponent<Button>().onClick.AddListener(redoButtonEvent);
     }
 
     public void AddButtonEvent()
     {
-        Command command = new AddCommand(prefObj);
-        command.excute();
+        Command add = new AddCommand(prefObj);
+        add.excute();
     }
 
     public void undoButtonEvent()
     {
+        Command.curCommand.undo();
+    }
 
+    public void redoButtonEvent()
+    {
+        Command.curCommand.redo();
     }
 }
